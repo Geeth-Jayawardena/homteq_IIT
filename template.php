6f14706957c922110dde84eef44dfc41486c6901
Geeth-Jayawardena <101686253+Geeth-Jayawardena@users.noreply.github.com>
2023-02-10 19:36:57 +0530

    February 10, 2023.TuteFinal

--- template.php
+++ template.php
@@ -1,10 +1,10 @@
 <?php
-$pagename="template"; //Create and populate a variable called $pagename
+$pagename = "template"; //Create and populate a variable called $pagename
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>"; //Call in stylesheet
-echo "<title>".$pagename."</title>"; //display name of the page as window title
+echo "<title>" . $pagename . "</title>"; //display name of the page as window title
 echo "<body>";
-include ("headfile.html"); //include header layout file 
-echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
+include("headfile.html"); //include header layout file 
+echo "<h4>" . $pagename . "</h4>"; //display name of the page on the web page
 //display random text
 echo "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
 labore et dolore magna aliqua. Non consectetur a erat nam at lectus urna. Cras pulvinar mattis nunc sed 
